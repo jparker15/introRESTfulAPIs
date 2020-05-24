9f85d415e1707ed62689ff39e3654d7cbcbb089d
Jeremy Parker <jeremyeparker@yahoo.com>
2020-05-23 23:53:02 -0400

    add logic to courses:id route

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require("express");
 //creates an express app
 const app = express();
 
+const courses = [
+    {id:1,name: "course1"},
+    {id:2,name: "course2"},
+    {id:3,name: "course3"},
+    {id:4,name: "course4"},
+    {id:5,name: "course5"},
+]
+
 app.get("/",(req,res) =>{
 
     res.send("Jello World");
@@ -11,13 +19,17 @@ app.get("/",(req,res) =>{
 
 app.get("/api/courses", (req,res) =>{
 
-    res.send([1,2,3,4,5]);
+    res.send(courses);
 
 });
 
-app.get("/api/posts/:year/:month", (req,res) =>{
+app.get("/api/courses:id", (req,res) =>{
 
-    res.send(req.query);
+  const course =  courses.find (c => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send("The course with given ID was not found");
+
+  res.send(course)
 
 });
 
