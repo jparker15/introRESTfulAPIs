ebf023090270603b554a7b44de1bc5f0e47e4c45
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-02 22:47:11 -0400

    set midware, connect database to Mongo

---
+++ introToMongo/index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+
+const express = require("express"),
+
+    server = express(),
+
+    morgan = require("morgan"),
+
+    mongoose = require("mongoose"),
+
+    port = process.env.PORT || 3001,
+
+    newObj = {useNewUrlParser: true, useUnifiedTopology: true},
+
+    connectionURI = process.env.MONGO;
+
+    const homeRouter = require("./routes/homeRouter");
+
+    // some midware needs to go before others
+    server.use(express.json());
+
+    server.use(morgan("dev"));
+    //express.json and morgan must be called before expressInstance.use
+
+    server.use("/", homeRouter);
+
+  
+
+   mongoose.connect(connectionURI,newObj ,() =>{
+
+  
+    console.log("The server is connected to the database");
+    
+
+   });
+   // event listener for error on connection
+   mongoose.connection.on("error", (err) =>{
+
+       console.log(`Error occured trying to connect to MongoDB,\nError:\n${err}`);
+       
+   })
+
+   mongoose.connection.on("connected", () =>{
+
+       console.log(`the server is attempting to connect to the database...`);
+       
+   })
+
+ 
+    // route listener
+server.listen( port, () => {
+
+    console.log(`server listening on port: ${port}`);
+    
+})
+
+    
