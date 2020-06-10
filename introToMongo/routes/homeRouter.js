b97c52ac4cfa3e45984c568820a8acfc9ecec48a
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-09 21:00:24 -0400

    add routes that use findMovie midware reduce DRY code

--- introToMongo/routes/homeRouter.js
+++ introToMongo/routes/homeRouter.js
@@ -95,49 +95,115 @@ router.get("/movie/:movieID", findMovie, (req,res) => {
         
     }
 });
+//my delete
+// router.delete("/movie/delete/:movieID",deleteMovie, (req, res) =>{
 
-router.delete("/movie/:movieID",deleteMovie, (req, res) =>{
+//     try{
 
-    try{
-
-        res.status(200).json({
-            status: 200,
-            message: "a movie was deleted",
-            movie: req.deletedMovie,
+//         res.status(200).json({
+//             status: 200,
+//             message: "a movie was deleted",
+//             movie: req.deletedMovie,
         
+//         });
+
+//     }
+//     catch (err) {
+//         console.log(err.message);
+
+//         res.status(500).json({
+//             status:500,
+//             message: "An error has occured durring DELETE Request",
+//             error: err.message
+//         });
+         
+//     }
+    
+    
+// });
+
+    //code is async by default
+    // declaring async allows the use of synchronus i.e await specifies a line as synchornus
+router.delete("/movie/delete/:movieID", findMovie, async (req,res) => {
+
+    console.log(req.params);
+    
+    try {
+                               //req param of route
+                // req obj. params obj 
+        //   let report = await Movie.findByIdAndDelete(req.params.movieID);
+        //   console.log(report);
+
+            await Movie.findByIdAndDelete(req.params.movie)
+                                  
+        res.status(200).json({
+            status:200,
+            deleted_movie: req.foundMovie
         });
-
-    }
-    catch (err) {
-        console.log(err.message);
-
+            
+        
+    } 
+    catch (err) {     //
+        console.error(`Error in HomeRouter` + err.message);
+        
         res.status(500).json({
             status:500,
-            message: "An error has occured durring DELETE Request",
-            error: err.message
-        });
-         
+            message:err.message
+        })
+
     }
-    
-    
 });
 
-router.patch("/movie/:movieID",updateMovie,(req,res) =>{
-        try{
-            res.status(200).json({
-                status: 200,
-                message: "movie has been updated",
-                movie: req.updatedMovie,
-            });
-        }
-        catch(err){
-            res.status(500).json({
-                status:500,
-                message: "an error occured during PATCH request",
-                error: err.message
-            });
-        }
-});
+router.patch("/movie/patch/:movieID", findMovie, async (req,res) =>{
+    
+    const id = req.params.movieID;
+    // keeps track of how many times a document has been updated
+    let newVersion = req.foundMovie.__v + 1;
+
+    req.body.__v = newVersion
+
+    try{
+        // doesn't not send updated document
+       await Movie.update({_id: id}, req.body);
+
+      const updatedDocument = await Movie.findById(id);
+
+       res.status(200).json({
+           status: 200,
+
+           new_document: updatedDocument,
+           old_document: req.foundMovie
+
+       })
+
+    }catch (err) {     //
+        console.error(`Error in HomeRouter` + err.message);
+        
+        res.status(500).json({
+            status:500,
+            message:err.message
+        })
+
+    }
+
+})
+
+// router.patch("/movie/:movieID",updateMovie,(req,res) =>{
+//         try{
+//             res.status(200).json({
+//                 status: 200,
+//                 message: "movie has been updated",
+//                 movie: req.updatedMovie,
+//             });
+//         }
+//         catch(err){
+//             res.status(500).json({
+//                 status:500,
+//                 message: "an error occured during PATCH request",
+//                 error: err.message
+//             });
+//         }
+// });
 
 router.post("/", async (req,res) =>{
                         //object constructor
