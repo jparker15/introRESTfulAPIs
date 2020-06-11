74c7876e533c4b7a0a72bfee991c154ab2a15b46
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-11 15:31:25 -0400

    require homeRouter, dotenv, morgan,, use morgan and homeRouter

--- intMongo2/app.js
+++ intMongo2/app.js
@@ -1,15 +1,20 @@
+require("dotenv").config(); 
+
 const express = require("express"),
+    mongoose = require("mongoose"),
+    morgan = require("morgan"),
+    home = require("./routes/homeRouter")
 
     app = express();
 
-    app.get("/", (req,res) =>{
-        res.send("test")
-    })
+    app.use(morgan("dev"));
+
+    app.use("/", home);
 
 
 
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5404;
     app.listen(port, () =>{
     
         console.log(`Listening Port ${port}`);
