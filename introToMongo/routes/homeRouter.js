41e52ed454c59f3753e392ea5e84bd7a5e08d1f8
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-18 22:31:43 -0800

    change handlers to render pug, /static unused html route

--- introToMongo/routes/homeRouter.js
+++ introToMongo/routes/homeRouter.js
@@ -2,16 +2,46 @@
         //express requirement
  const express = require("express"),
         //instance of express' Router()
-    router = express.Router();
+    router = express.Router(),
         //model for Movie requirement
+    Movie = require("../models/Movie")
 
-        router.get("/", (req,res) => {
+
+        //pug 
+        // router.get("/", (req,res) =>{
+
+        //     res.render("test",{message:"Test msg",titleVar: "Title tester"} )
+            
+        // });
+
+        router.get("/mrental", async (req,res) =>{
+            const allMovies = await Movie.find({}),
+
+                clientMsg = `Number of Movies:` + allMovies.length; 
+
+            res.render("home", {all_movies: allMovies, message: clientMsg} )
+        })
+
+
+        router.get("/mrental/new", async (req,res) =>{
+
+            res.render("newMovie")
+
+        })
+
+        //html work in progress
+
+        router.get("/mrental/static", (req,res) => {
             //cwd is directory of whole project 
 
-            const fileLoc = process.cwd() + `\\public\\home-static\\home.html`;
+            console.log("connecting maybe....");
+            
+
+            const fileLoc = process.cwd() + `\\public\\home-static\\homePage\\home.html`;
             
             res.sendFile(fileLoc);
         });
+
 
 
 
