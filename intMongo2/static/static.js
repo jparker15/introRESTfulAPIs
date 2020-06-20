808b1ac86e0aac920f38c133706f314c50c0ab4f
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-19 19:55:00 -0800

    add xhr to load function (wip load function name)

--- intMongo2/static/static.js
+++ intMongo2/static/static.js
@@ -1,6 +1,32 @@
-window.onload = () =>{
-    const initDiv = document.createElement("div");
-        initDiv.id = "initDiv";
 
-    document.body.appendChild(initDiv);
+window.onload = () =>{
+    
+    createInitElems()
+
+    Load()
+}
+
+function Load () {
+
+    const xhr = new XMLHttpRequest(),
+    
+        endpoint = `${location}all`;
+
+        xhr.open("GET",endpoint,true);
+
+        xhr.onload = () => {
+            
+            const parsedData = JSON.parse(xhr.responseText);
+
+            console.log(parsedData.entries);
+
+
+            
+        }
+
+        xhr.send();
+}
+
+function postEntry (entry) {
+    
 }
