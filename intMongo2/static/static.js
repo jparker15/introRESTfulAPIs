ed9a8c562686157527479771bc8635c79968f3fe
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-22 16:53:36 -0400

    change className, add editCard and updateGPU functions

--- intMongo2/static/static.js
+++ intMongo2/static/static.js
@@ -1,12 +1,12 @@
 
 window.onload = () =>{
-    const deleteBtns = document.getElementsByClassName("deleteCard");
+    const deleteBtns = document.getElementsByClassName("deleteGPU");
 
     for(const button of deleteBtns){
         button.onclick = deleteCard;
     }
 
-    const editBtns = document.getElementsByClassName("editCard");
+    const editBtns = document.getElementsByClassName("patchGPU");
 
     for(const button of editBtns){
         button.onclick = editCard;
@@ -23,8 +23,8 @@ function postPage () {
 
 }
 
-function deleteCard () {
-
+function deleteCard () {   
+    
     const cardID = this.parentElement.id,
 
           endpoint = `${location.origin}/delete/${cardID}`,
@@ -48,5 +48,28 @@ function deleteCard () {
 
 function editCard () {
 
+        console.log(this.parentElement.id);
+        
+
 }
 
+function updateGPU (){
+    const cardID = this.parentElement.id,
+
+    endpoint = `${location.origin}/update/${cardID}`,
+
+    reqObj = {method:"PATCH"};
+
+fetch(endpoint,reqObj)
+.then(rs=>{return rs.json()})
+.then(res =>{
+    console.log(res);
+
+    
+
+    window.location.reload();
+
+    
+})
+
+}
