c8679a5196d16665fb9de4986f1e373162ae154c
Jeremy Parker <jeremyeparker@yahoo.com>
2020-06-23 17:52:58 -0400

    change home page to render query params, add authorizer, add admin route

--- introToMongo/routes/homeRouter.js
+++ introToMongo/routes/homeRouter.js
@@ -4,15 +4,39 @@
         //instance of express' Router()
     router = express.Router(),
         //model for Movie requirement
-    Movie = require("../models/Movie")
+    Movie = require("../models/Movie"),
+
+    adminAuthrzr = require("../middleware/adminAuthrzr")
 
 
         //pug 
-        // router.get("/", (req,res) =>{
+        router.get("/", (req,res) =>{
+            //expected query properties: "msg" and "title"
 
-        //     res.render("test",{message:"Test msg",titleVar: "Title tester"} )
+                //location.origin/?msg="message"&title="new title"
+
+                    //deconstructed object
+
+                    //expected query in route
+            const {msg,title} = req.query;
+
+            console.log(msg,title);
+
+
+            res.render("test",{
+
+                message:msg || "default msg",
+
+                titleVar:title || "default title"
             
-        // });
+            });
+            
+        });
+
+        // router.get("/:request params/:reqParams",(req,res) =>{
+        //     location = `${location.origin}/mrental`
+        //     res.send("yah")
+        // })
 
         router.get("/mrental", async (req,res) =>{
             const allMovies = await Movie.find({}),
@@ -20,14 +44,24 @@
                 clientMsg = `Number of Movies:` + allMovies.length; 
 
             res.render("home", {all_movies: allMovies, message: clientMsg} )
-        })
+        });
 
 
-        router.get("/mrental/new", async (req,res) =>{
+        // router.get("/mrental/new", async (req,res) =>{
 
-            res.render("newMovie")
+        //     res.render("newMovie")
 
-        })
+        // });
+
+        // router.get("/mrental/update",(req,res) =>{
+        //     res.render("updateMovie")
+        // });
+
+        router.get("/mrental/admin/:key",adminAuthrzr,(req,res) =>{
+
+            res.render("admin-movie")
+
+        });
 
         //html work in progress
 
