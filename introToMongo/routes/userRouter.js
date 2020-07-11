1910edbf3acc449eb34733f0762db8a44d900768
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-10 23:33:03 -0400

    add requirements, midware, .env, update routes

--- introToMongo/routes/userRouter.js
+++ introToMongo/routes/userRouter.js
@@ -1,36 +1,67 @@
 const User = require("../models/User");
-
-// const express = require("express");
-// const router = express.Router();
-
 const router = require("express").Router();
 const validator = require("validator");
-const validUser = require("../middleware/validUser")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validUser = require("../middleware/validUser");
+const authUser = require("../middleware/authUser");
+const secret = process.env.JWT_SECRET;
+
+
 //POST route for Users
-//
+//localhost:3015/user/
 //@desc post/make a new user and store in users collections
-//@path (server path)/user/postlocalhost:3015/user
-//@access admin level 
-router.post("/", validUser, async(req,res) =>{
-   
-   
-    try {
+//@path (server path)/user/
+//@access public
 
-       const newUser = await User.create(req.body);
+router.post(
+    "/",
+    validUser
+    ,async (req,res) =>{
+    //block a user from bypassing admin level and isAdmin
 
-        res.json({
-            msg: "user created",
-            document:newUser
-        })
-        
-    } catch (error) {
-        res.status(500).json({
-            status:500,
-            error:error.message || error
-        })
-    }
+    //encrypt password for safe DB storage
     
-})
+    const salt = await bcrypt.genSalt(7);
+
+    const hashedPass = await bcrypt.hash( req.body.password, salt);
+    console.log(salt, bcrypt.getRounds(hashedPass));
+    req.body.password = hashedPass
+
+        try {
+
+            const newUser = await User.create(req.body);
+     
+             res.json({
+                 msg: "user created",
+                 document:newUser
+             })
+             
+         } catch (error) {
+             res.status(500).json({
+                 status:500,
+                 error:error.message || error
+             })
+         }
+}) 
+
+//PATCH (login) route for Users
+// localhost:3015/user
+//@desc put/login a new user and store in users collections
+//@path (server path)/user/post 
+//@access public
+
+router.patch(
+    "/",
+    authUser,
+    async(req,res) =>{
+
+        const token = jwt.sign({id: req.id},secret);
+
+        res.json(token)
+    
+    }    
+)
 
 
 
