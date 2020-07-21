b3640c33422915367db0dbe5314b3fd40100498e
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-20 22:02:46 -0400

    require jwt, set alias for .env keys, set to find adminProp.isAdmin query for verification from mongoDB

--- introToMongo/middleware/adminAuth.js
+++ introToMongo/middleware/adminAuth.js
@@ -1,11 +1,49 @@
 const apiKey = process.env.MRENTAL_ADMIN;
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+module.exports = async (req,res,next) => {
 
-function adminAuthrzr(req,res,next){
+   const {
+       JWT_SECRET: jwtKey,
+       HEAD_AUTH_KEY: headerKey
+   } = process.env;
 
+   const userTkn = req.headers[headerKey];
+
+//    console.log(userTkn);
+   
+   try {//  jwt verifing userToken
+       const decodedData = jwt.verify(userTkn,jwtKey);
+
+       if(decodedData.id === undefined && decodedData.id.length != 24){
+        throw new Error ("ID was not defined in the payload or length was invalid");
+    }
+
+       const query = {_id: decodedData.id, "adminProp.isAdmin":true},
+            //info to include or exclude 
+            projection = {password:0, __V: 0,}
     
-    next()
+      const adminInfo = await User.findOne(
+            query,projection
+          );
 
+        if(adminInfo === null) throw new Error ("user is not an admin")
+
+       req.admin = adminInfo;
+
+       next()
+
+   } 
+   catch (error) {
+    
+        const errMsg = error.message || error;
+        //error for dev
+        console.error(`\nerror userAdmin: ${errMsg}\n`);
+        //error for end user
+        return res.status(401).json({
+            
+            status:401,
+            message: "Not Authorized",
+        });
+   }
 }
-
-module.exports = adminAuthrzr;
