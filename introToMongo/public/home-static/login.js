093f89d28bf3db34cd243cefbe0099b51e51f7a6
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-24 22:45:43 -0400

    fetch req for login input

---
+++ introToMongo/public/home-static/login.js
@@ -0,0 +1,50 @@
+window.onload=()=>{
+
+    document.getElementById("submitLogin").onclick = () =>{
+    
+       const email = document.getElementById("email").value;
+
+       const password = document.getElementById("password").value;
+
+        const reqBody = {
+
+            email: email,
+            password: password,
+
+        };
+        const endpoint = `${location.origin}/user/login`;
+
+        const reqOptions = {
+            headers: {
+
+                Accept: "application/json",
+
+                "content-type": "application/json",
+            },
+            method:"PATCH",
+            body: JSON.stringify(reqBody)
+        }
+
+        fetch(endpoint,reqOptions)
+        .then(rs=>{
+            console.log(rs);
+            return rs.json()
+        })
+        .then(res =>{
+
+            const token = res.token; 
+        
+            if(token === undefined){
+                alert("Login Failed")
+            }
+            else{
+                localStorage.setItem("loginToken",token);
+
+                location = `${location.origin}`;
+            }
+            //console.log(res);
+        })
+        
+
+    }
+}
