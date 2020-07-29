23b85fee19afa899b0208f52ea5f3fc227ff4bf8
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-29 11:29:15 -0400

    use of jwt verify on req.authKey and check if id has admin props

---
+++ introToMongo/middleware/isAdmin.js
@@ -0,0 +1,45 @@
+const User = require("../models/User");
+
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req,res,next)=> {
+
+  
+    // console.log(req.authKey);
+
+    const {JWT_SECRET: jwtKey} = process.env;
+
+    const usrTkn = req.authKey;
+
+    try {
+
+        const decryptedId  = jwt.verify(usrTkn,jwtKey);
+
+        const adCheck = await User.findOne({_id:decryptedId.id},{"adminProp.isAdmin": true});
+
+        // if(adCheck !== null){
+        //     req.isAdmin = true
+        // }
+
+        // console.log(adCheck);
+
+        // if(adCheck.adminProp.isAdmin == true){
+        //     console.log("is an admin");
+        // } else{
+        //     console.log("not an admin");
+        // }
+            
+            req.isAdmin = true === adCheck.adminProp.isAdmin; 
+
+            // console.log(req.isAdmin);
+
+        next()
+
+    } catch (err) {
+        console.log(err.message||err);
+        next()
+    }
+
+    
+
+}
